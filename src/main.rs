2b6f6762ac01304137cbf66494789a857f26ce0d
Jose Paredes <jozemparedes@gmail.com>
2022-06-13 16:31:05 -0600

    Configuring actix

--- src/main.rs
+++ src/main.rs
@@ -10,52 +10,29 @@ use std::env;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 
-fn main() {
+use diesel::r2d2::Pool;
+use diesel::r2d2::{self, ConnectionManager};
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    let db_url = env::var("DATABASE_URL").expect("DB Url not found.");
+    let connection = ConnectionManager::<PgConnection>::new(db_url);
+    let pool = Pool::builder()
+        .build(connection)
+        .expect("Unable to create the connection pool.");
 
-    let db_url = env::var("DATABASE_URL").expect("DB URL not found");
-    let conn = PgConnection::establish(&db_url).expect("Error: Unable to connecto to data base");
-
-    use self::models::{NewPost, Post, PostSimplified};
-    use self::schema::posts;
-    use self::schema::posts::dsl::*;
-
-    // let new_post = NewPost {
-    //     title: "Third Blog Post",
-    //     body: "3 This is the body of the blobpost",
-    //     slug: "third-blog-post",
-    // };
-
-    // diesel::insert_into(posts::table)
-    //     .values(new_post)
-    //     .get_result::<Post>(&conn)
-    //     .expect("Falied: Data insertion failed");
-
-    //diesel::update(posts.filter(id.eq(3))).set(slug.eq("post-modified")).get_result::<Post>(&conn).expect("Unable to modify post");
-
-    //let post_del = diesel::delete(posts.filter(id.eq(3))).get_result::<Post>(&conn).expect("Unable to remove post");
-    
-    println!("Post removed: {}", post_del.title);
-
-    // Get all posts
-    let res_posts = posts
-        .limit(10)
-        .load::<Post>(&conn)
-        .expect("Error: Unable to retrieve posts.");
-
-    for post in res_posts {
-        println!(
-            "id: {} - Title:{} - Slug: {}",
-            post.id, post.title, post.slug
-        )
-    }
-
-    let post_titles = posts
-        .select((title, id))
-        .load::<PostSimplified>(&conn)
-        .expect("Unable to retrieve posts");
-    println!("Just titles and ids");
-    for post in post_titles {
-        println!("{}", post.title);
-    }
+    HttpServer::new(move || App::new()
+        .service(hello_world).app_data(pool.clone()))
+        .bind(("127.0.0.1", 8000))
+        .unwrap()
+        .run()
+        .await
 }
