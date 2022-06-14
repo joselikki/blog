cdf0e4069aa70d756650353faa615230dd941f4f
Jose Paredes <jozemparedes@gmail.com>
2022-06-14 16:53:06 -0600

    template engine

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ extern crate diesel;
 pub mod models;
 pub mod schema;
 
+//use serde_json::StreamDeserializer;
+use tera::Tera;
+
 use dotenv::dotenv;
 use std::env;
 
@@ -18,16 +21,60 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 pub type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
 
 use self::models::{NewPostHandler, Post};
-//use self::schema::posts;
+use self::schema::posts;
 use self::schema::posts::dsl::*;
 
 #[get("/")]
-async fn index(pool: web::Data<DbPool>) -> impl Responder {
+async fn index(pool: web::Data<DbPool>, template_manager: web::Data<tera::Tera>) -> impl Responder {
     let conn = pool.get().expect("Unable to connect with DB");
 
     match web::block(move || posts.load::<Post>(&conn)).await {
-        Ok(data) => HttpResponse::Ok().body(format!("{:?}", data)),
-        Err(_err) => HttpResponse::Ok().body("There was an error retrieving the data"),
+        Ok(data) => {
+            let data = data.unwrap();
+            let mut context = tera::Context::new();
+            context.insert("posts", &data);
+
+            return HttpResponse::Ok()
+                .content_type("text/html")
+                .body(template_manager.render("index.html", &context).unwrap());
+        }
+        Err(_err) => {
+            return HttpResponse::Ok().body("There was an error retrieving the data");
+        }
+    }
+}
+
+#[get("/blog/{blog_slug}")]
+async fn get_post(
+    pool: web::Data::<DbPool>, 
+    template_manager: web::Data<tera::Tera>, 
+    request : web::Path<String> 
+) -> impl Responder {
+
+    let conn = pool.get().expect("Unable to connect with DB");
+    let post_slug = request.into_inner();
+
+    match web::block(move || {posts.filter(slug.eq(&post_slug)).load::<Post>(&conn)}).await {
+
+        Ok(data) => {
+            let data = data.unwrap();
+
+            if data.len() == 0 {
+                return HttpResponse::NotFound().finish();
+            }
+
+            let data = &data[0];
+
+            let mut context = tera::Context::new();
+            context.insert("post", data);
+
+            HttpResponse::Ok().content_type("text/html").body(
+                template_manager.render("blog.html", &context).unwrap())
+            
+        }
+        Err(_err)=> {
+            return HttpResponse::Ok().body("Unable to get data");
+        }
     }
 }
 
@@ -51,10 +98,14 @@ async fn main() -> std::io::Result<()> {
         .expect("Unable to create the connection pool.");
 
     HttpServer::new(move || {
+        let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
+
         App::new()
             .service(index)
             .service(new_post)
+            .service(get_post)
             .data(pool.clone())
+            .app_data(web::Data::new(tera))
     })
     .bind(("127.0.0.1", 8000))
     .unwrap()
